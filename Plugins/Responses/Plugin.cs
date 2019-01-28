4322ebf882bffe2f0eb5363aff14b64f193ae934
AlienXAXS <mark.phillips@axsemail.co.uk>
2019-01-28 18:52:56 +0000

    Add AuthCommands back in.

--- Plugins/Responses/Plugin.cs
+++ Plugins/Responses/Plugin.cs
@@ -24,7 +24,7 @@ namespace Responses
             // Register our commands with the handler
             CommandHandler.HandlerManager.Instance.RegisterHandler<Commands.AdminCommands>();
             CommandHandler.HandlerManager.Instance.RegisterHandler<Informational.LastOnline>();
-            //CommandHandler.HandlerManager.Instance.RegisterHandler<Commands.AuthorisedCommands>();
+            CommandHandler.HandlerManager.Instance.RegisterHandler<Commands.AuthorisedCommands>();
         }
 
         Task IPlugin.CommandAsync(string command, string message, SocketMessage sktMessage)
