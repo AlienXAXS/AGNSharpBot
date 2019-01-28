bb6ff81f3e3cae832674c6936e7762a0617a620d
AlienXAXS <mark.phillips@axsemail.co.uk>
2019-01-28 19:31:45 +0000

    Wrote CheckPermission method to check for a users permission against db

--- PermissionHandler/Permission.cs
+++ PermissionHandler/Permission.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Discord.Commands;
 using Discord.WebSocket;
 using GlobalLogger;
+using PermissionHandler.DB;
 
 namespace PermissionHandler
 {
@@ -26,28 +28,61 @@ namespace PermissionHandler
 
             foreach (var thisMethod in t.GetMethods())
             {
-                var requiredAttributeFound = thisMethod.GetCustomAttributesData().Any(x => x.AttributeType.Name.Equals("Command"));
+                var requiredAttributeFound =
+                    thisMethod.GetCustomAttributesData().Any(x => x.AttributeType.Name.Equals("Command"));
 
                 if (requiredAttributeFound)
                 {
                     var pathName = $"{typeof(T)}.{thisMethod.Name}";
                     _registeredPermissionPaths.Add(pathName);
                     _database.AddPermission(pathName);
-                    Logger.Instance.Log($"Dynamically registered a new permission path node: {pathName}", Logger.LoggerType.ConsoleAndDiscord).Wait();
+                    Logger.Instance.Log($"Dynamically registered a new permission path node: {pathName}",
+                        Logger.LoggerType.ConsoleAndDiscord);
                 }
             }
         }
 
+        public List<string> GetRegisteredPermissionPaths()
+        {
+            return _registeredPermissionPaths;
+        }
+
         public bool CheckPermission(SocketGuildUser sktUser,
             [System.Runtime.CompilerServices.CallerMemberName]
-            string memberName = "")
+            string path = "")
         {
             if (sktUser.Roles.Any(x => x.Permissions.Administrator))
-            {
                 return true;
+
+            var permissionNode = _database.GetData().DefaultIfEmpty(null).FirstOrDefault(x => x.Path.Equals(path));
+            if (permissionNode == null)
+                throw new Exception(
+                    $"Possible unknown permission path with {path}, user {sktUser.Username} attempted a command that I did not recognise");
+
+            var canPermit = false;
+            var foundExplicitDeny = false;
+
+            // Check users roles
+            foreach (var role in sktUser.Roles)
+            {
+                var rolePerm = permissionNode.Permissions.DefaultIfEmpty(null)
+                    .FirstOrDefault(x => x.Owner.Equals(role.Id));
+                switch (rolePerm.Permission)
+                {
+                    case NodePermission.Allow:
+                        canPermit = true;
+                        break;
+                    case NodePermission.Deny:
+                        foundExplicitDeny = true;
+                        break;
+                }
             }
 
-            return false;
+            // Find explicit user deny
+            foundExplicitDeny = permissionNode.Permissions
+                .Any(x => x.Owner.Equals(sktUser.Id) && x.Permission == NodePermission.Deny);
+
+            return canPermit && !foundExplicitDeny;
         }
     }
 }
