d066dc2ec06cbc1cf4a814147c11ec24b3ec3026
AlienXAXS <mark.phillips@axsemail.co.uk>
2019-02-09 20:42:43 +0000

    Revert "Removed unused async from some methods"
    This reverts commit a066390290e6930cfe33189bf6ad6d65e7c6e50a.

--- PermissionHandler/DB/Database.cs
+++ PermissionHandler/DB/Database.cs
@@ -95,39 +95,31 @@ namespace PermissionHandler
         #region Save / Load Functionality
         public void Load()
         {
-            lock (_nodes)
+            try
             {
-                try
-                {
-                    if (!System.IO.File.Exists(StorageLocation))
-                        throw new Exception(
-                            "Unable to load permissions database as the database has yet to be used, this error should go away once you use it");
-                    _nodes = JsonConvert.DeserializeObject<List<Node>>(System.IO.File.ReadAllText(StorageLocation));
-                }
-                catch (Exception ex)
-                {
-                    Logger.Instance.Log(
-                        $"[THREAD {System.Threading.Thread.CurrentThread.ManagedThreadId}] Unable to load permission database, error follows:\r\n{ex.Message}\r\n{ex.StackTrace}",
-                        Logger.LoggerType.ConsoleAndDiscord).Wait();
-                }
+                if ( !System.IO.File.Exists(StorageLocation) )
+                    throw new Exception("Unable to load permissions database as the database has yet to be used, this error should go away once you use it");
+                _nodes = JsonConvert.DeserializeObject<List<Node>>(System.IO.File.ReadAllText(StorageLocation));
+            }
+            catch (Exception ex)
+            {
+                Logger.Instance.Log(
+                    $"[THREAD {System.Threading.Thread.CurrentThread.ManagedThreadId}] Unable to load permission database, error follows:\r\n{ex.Message}\r\n{ex.StackTrace}",
+                    Logger.LoggerType.ConsoleAndDiscord).Wait();
             }
         }
 
         public void Save()
         {
-            lock (_nodes)
+            try
             {
-                try
-                {
-                    System.IO.File.WriteAllText(StorageLocation,
-                        JsonConvert.SerializeObject(_nodes, Formatting.Indented));
-                }
-                catch (Exception ex)
-                {
-                    Logger.Instance.Log(
-                        $"[THREAD {System.Threading.Thread.CurrentThread.ManagedThreadId}] Unable to save permission database, error follows:\r\n{ex.Message}\r\n{ex.StackTrace}",
-                        Logger.LoggerType.ConsoleAndDiscord).Wait();
-                }
+                System.IO.File.WriteAllText(StorageLocation, JsonConvert.SerializeObject(_nodes, Formatting.Indented));
+            }
+            catch (Exception ex)
+            {
+                Logger.Instance.Log(
+                    $"[THREAD {System.Threading.Thread.CurrentThread.ManagedThreadId}] Unable to save permission database, error follows:\r\n{ex.Message}\r\n{ex.StackTrace}",
+                    Logger.LoggerType.ConsoleAndDiscord).Wait();
             }
         }
         #endregion
