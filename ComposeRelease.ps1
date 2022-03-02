28c318c7c8af0774360cf57d34015d858337de1b
AlienXAXS <mark.phillips@axsemail.co.uk>
2022-03-02 19:17:16 +0000

    Added Pre-Build Ps scripts

---
+++ ComposeRelease.ps1
@@ -0,0 +1,25 @@
+Set-Location ".."
+$currentPath = Get-Location
+Write-Host "Script running in location $currentPath"
+
+$plugins = Get-Item -Path ".\Plugins\Binaries\*"
+$pluginNames = Get-Item -Path ".\AGNSharpBot_v2\bin\Release\netcoreapp3.1\Plugins\*" -Filter "*.dll"
+$destPath = ".\AGNSharpBot_v2\bin\Release\netcoreapp3.1\"
+
+foreach ( $plugin in $plugins )
+{
+    $pluginPath = $plugin.FullName + "\bin\Release\netcoreapp3.1\*"
+    if ( [System.IO.Directory]::Exists($pluginPath) )
+    {
+        $files = Get-Item -Path $pluginPath -Filter "*.dll" | Where-object { $pluginNames.Name -notcontains $_.Name }
+
+        foreach ( $file in $files )
+        {
+            if ( -not [System.IO.File]::Exists("$destPath\$($file.Name)") )
+            {
+                Copy-Item -Path $file.FullName -Destination $destPath
+                Write-Host "Copied $($file.Name) to main directory";
+            }
+        }
+    }
+}
