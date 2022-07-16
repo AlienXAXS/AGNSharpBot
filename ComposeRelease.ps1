82eb3b0704559e058fa3245eff7d94dd7345d7a2
AlienXAXS <mark.phillips@axsemail.co.uk>
2022-07-16 17:30:52 +0100

    Updated ComposeRelease so it actually works now lul.

--- ComposeRelease.ps1
+++ ComposeRelease.ps1
@@ -1,25 +1,32 @@
-Set-Location ".."
+Set-Location "C:\Users\AlienX\Documents\GitHub\AGNSharpBot"
 $currentPath = Get-Location
 Write-Host "Script running in location $currentPath"
 
 $plugins = Get-Item -Path ".\Plugins\Binaries\*"
 $pluginNames = Get-Item -Path ".\AGNSharpBot_v2\bin\Release\netcoreapp3.1\Plugins\*" -Filter "*.dll"
-$destPath = ".\AGNSharpBot_v2\bin\Release\netcoreapp3.1\"
+$destPath = "\AGNSharpBot_v2\bin\Release\netcoreapp3.1\"
 
 foreach ( $plugin in $plugins )
 {
-    $pluginPath = $plugin.FullName + "\bin\Release\netcoreapp3.1\*"
+    $pluginPath = $plugin.FullName + "\bin\Release\netcoreapp3.1"
     if ( [System.IO.Directory]::Exists($pluginPath) )
     {
-        $files = Get-Item -Path $pluginPath -Filter "*.dll" | Where-object { $pluginNames.Name -notcontains $_.Name }
+        $filesToMain = Get-Item -Path "$pluginPath\*" -Filter "*.dll" | Where-object { $pluginNames.Name -notcontains $_.Name }
+        $pluginFile = Get-Item -Path "$pluginPath\*" -Filter "*.dll" | Where-object { $pluginNames.Name -contains $_.Name }
 
-        foreach ( $file in $files )
+        foreach ( $file in $filesToMain )
         {
-            if ( -not [System.IO.File]::Exists("$destPath\$($file.Name)") )
+            if ( [System.IO.File]::Exists("$currentPath" + "$destPath$($file.Name)") -eq $false )
             {
                 Copy-Item -Path $file.FullName -Destination $destPath
-                Write-Host "Copied $($file.Name) to main directory";
+                Write-Host "Copied $($file.Name) library to Main Working Directory";
             }
+        }
+
+        foreach ( $file in $pluginFile )
+        {
+            Copy-Item -Path $file.FullName -Destination ("$currentPath" + "$destPath\Plugins\")
+            Write-Host "Copied $($file.Name) library to Plugin Directory";
         }
     }
 }
